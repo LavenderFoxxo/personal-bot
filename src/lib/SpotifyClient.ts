3618297896f84d9214df827b3e4e86bc81e01c76
Alexander Hyman <alexander.hyman05@gmail.com>
2023-06-10 17:20:18 -0700

    fix: new domain

--- src/lib/SpotifyClient.ts
+++ src/lib/SpotifyClient.ts
@@ -3,5 +3,5 @@ import SpotifyWebApi from 'spotify-web-api-node';
 export const spoitfyApi = new SpotifyWebApi({
     clientId: "a0f873411e774fe5af38a22b7c4b52cb",
     clientSecret: "69fa64bce12749f99575759668715741",
-    redirectUri: "https://bot.itsalexander.dev/callback"
+    redirectUri: "https://bot.lavy.ws/callback"
 });
